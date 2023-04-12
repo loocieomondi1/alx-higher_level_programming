12756c3e8f60b6c7e2f011817f190bd1db5366ba
loocieomondi <loocieomondi@gmail.com>
2023-04-12 09:07:11 +0000

    input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+module to read a file and print it to the stdout
+"""
+
+
 def read_file(filename=""):
+    """
+    read_file
+    args
+        path to file to be read
+    """
     with open(filename, encoding='utf-8') as f:
         for line in f:
             print(line, end='')
